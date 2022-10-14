0ef2c77a03070f05bd9a4de476f46c1f7e9babe2
andrieli <andrieliluci@gmail.com>
2022-10-13 22:26:39 -0300

    create pages

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder 
 from kivy.uix.screenmanager import ScreenManager, Screen 
 
-class Home(Screen):
-    pass
-
-class Guide(Screen):
-    pass
-
-class NewReading(Screen):
-    pass
+import pages.Home as Home
+import pages.Guide as Guide
+import pages.NewReading as NewReading
 
 class WindowManager(ScreenManager):
     pass
-
 
 kv = Builder.load_file('main.kv')
 
