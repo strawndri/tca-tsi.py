b8dd5bb6ae41b8ce92dfdf51497371ffe2b7f8e9
andrieli <andrieliluci@gmail.com>
2022-10-15 17:06:10 -0300

    finish home style

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder 
 from kivy.uix.screenmanager import ScreenManager, Screen 
+from kivy.core.window import Window
 
 import pages.Home as Home
 import pages.Guide as Guide
@@ -10,6 +11,7 @@ import pages.NewReading as NewReading
 class WindowManager(ScreenManager):
     pass
 
+Window.size = (1200, 600)
 kv = Builder.load_file('main.kv')
 
 class MainApp(App):
