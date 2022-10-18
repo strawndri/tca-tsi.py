033c88b82210025d24e60a6dbf2b074d09c05e3b
andrieli <andrieliluci@gmail.com>
2022-10-18 10:45:30 -0300

    create a button to copy text to the clipboard

--- pages/NewReading.py
+++ pages/NewReading.py
@@ -2,11 +2,14 @@ from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from kivy.uix.screenmanager import Screen 
 
+new_window = Tk()
+Tk.withdraw(new_window)
+
 class NewReading(Screen):
 
     checks = []
     filename = './img/img-icon.png'
-    
+
     def checkbox_click(self, instance, value, topping):
         if value == True:
             NewReading.checks.append(topping)
@@ -20,8 +23,13 @@ class NewReading(Screen):
                 tops = f'{tops} {i}'
 
     def uploadFile(self):
-        new_window = Tk()
-        Tk.withdraw(new_window)
         filename = askopenfilename()
 
         self.ids.imageToAnalyse.source = filename
+
+    def copy_text(self):
+        new_window.clipboard_clear()
+        new_window.clipboard_append(self.ids.result.text)
+        new_window.update()
+        new_window.destroy()
+
