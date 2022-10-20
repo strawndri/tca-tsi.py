937bbac5b8423ddbf750748618848d61f7e5d6ef
andrieli <andrieliluci@gmail.com>
2022-10-19 21:54:08 -0300

    add download button

--- pages/NewReading.py
+++ pages/NewReading.py
@@ -1,4 +1,6 @@
 from tkinter import Tk
+from fpdf import FPDF
+from pathlib import Path
 from tkinter.filedialog import askopenfilename
 from kivy.uix.screenmanager import Screen 
 
@@ -32,10 +34,18 @@ class NewReading(Screen):
         self.ids.imageToAnalyse.source = path
         self.ids.link.text =  ''
         
-
     def copy_text(self):
         new_window.clipboard_clear()
         new_window.clipboard_append(self.ids.result.text)
         new_window.update()
         new_window.destroy()
+
+    def string_to_pdf(self):
+        pdf = FPDF()
+        download_path = str(Path.home()/"Downloads")
+        path = download_path + '/test.pdf'
+        pdf.add_page()
+        pdf.set_font('Arial', 'B', 12)
+        pdf.cell(40, 10, self.ids.result.text)
+        pdf.output(path, 'F')
 
