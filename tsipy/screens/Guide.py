5d3dd9540884c6c53df2148626cbaa8a7f9be749
andrieli <andrieliluci@gmail.com>
2022-11-13 10:47:44 -0300

    import guide

--- tsipy/screens/Guide.py
+++ tsipy/screens/Guide.py
@@ -1,5 +1,29 @@
 from kivy.uix.screenmanager import Screen 
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 class Guide(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(Guide, self).__init__(**kwargs)
+        self.inside = BoxLayout()
+        with open('docs/text/guide.txt', encoding='utf8') as file:
+            for line in file:
+                match (line[0]):
+                    case '1':
+                        self.inside.add_widget(Label(text=line))
+                    case '2':
+                        self.inside.add_widget(Label(text=line))
+                    case '3':
+                        self.inside.add_widget(Label(text=line))
+                    case '4':
+                        self.inside.add_widget(Label(text=line))
+                    case '5':
+                        self.inside.add_widget(Label(text=line))
+                    case '6':
+                        self.inside.add_widget(Label(text=line))
+    
+
+
+    
+
 
